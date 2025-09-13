27d15ef5717cd8f207b8ee9aa86dc32cd0468e09
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-13 13:15:57 +0530

    design plantCategories section

--- components/sections/plantCategories.tsx
+++ components/sections/plantCategories.tsx
@@ -1,13 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { plantCategories } from '@/constants/sampleData'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const PlantCategories = () => {
+  const [categories] = useState(plantCategories)
 
   return (
     <View>
-      <Text>Plant Categories</Text>
+      <Text style={styles.heading}>Plant Categories</Text>
+        {/* Render plant categories here */}
+        {categories.map(category => (
+          <View key={category.id}>
+           <LinearGradient style={styles.linearGradient} colors={['#72f7d1', '#014a35', '#001a12']}>
+            <TouchableOpacity>
+                <Text style={styles.buttonText}>{category.name}</Text>
+            </TouchableOpacity>
+           </LinearGradient> 
+          </View>
+        ))}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    buttonText: {
+        fontSize: 18,
+        color: '#ffffff',
+        fontWeight: 'bold',
+        textAlign: 'left',
+    },
+    heading: {
+        marginTop: 30,
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#001a12',
+        marginBottom: 10,
+        textAlign: 'left',
+        paddingLeft: 10,
+    },
+    linearGradient: {
+        margin: 10,
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 30,
+        paddingRight: 30,
+        width: '80%',
+        alignSelf: 'center',
+        borderRadius: 50,
+        flexDirection: 'column',
+    },
+})
 
 export default PlantCategories
---
+++ constants/sampleData.ts
@@ -0,0 +1,8 @@
+// Categories of plants
+  export const plantCategories = [
+    { id: 1, name: 'Indoor'},
+    { id: 2, name: 'Outdoor'},
+    { id: 3, name: 'Flowering'},
+    { id: 4, name: 'Indoor and Outdoor'},
+    { id: 5, name: 'Cacti'},
+  ];
