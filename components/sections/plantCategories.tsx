3156e5c9576a34491305d5ce76dfb504ef56e9a0
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-13 11:49:14 +0530

    plant category section

---
+++ components/sections/plantCategories.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PlantCategories = () => {
+
+  return (
+    <View>
+      <Text>Plant Categories</Text>
+    </View>
+  )
+}
+
+export default PlantCategories
