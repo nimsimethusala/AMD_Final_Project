5ca047fd17aeae6acac2e4d1882d96ab6b29d601
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-13 11:42:56 +0530

    add header section into the home page

--- app/(Dashboard)/home.tsx
+++ app/(Dashboard)/home.tsx
@@ -1,9 +1,11 @@
+import HeaderSection from '@/components/sections/headerSection';
 import React from 'react';
 import { View, Text } from 'react-native';
 
 const Home = () => {
   return (
     <View>
+      <HeaderSection />
       <Text>Home Screen</Text>
     </View>
   );
